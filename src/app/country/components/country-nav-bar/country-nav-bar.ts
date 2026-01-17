9e767bf96f8b83694204614f8c8fc3bab944d252
alejosv07 <balrking07@gmail.com>
2026-01-16 16:35:04 -0800

    add contry-navbar

---
+++ src/app/country/components/country-nav-bar/country-nav-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'country-nav-bar',
+  imports: [],
+  templateUrl: './country-nav-bar.html',
+  styles: ``,
+})
+export class CountryNavBar {
+
+}
--- src/app/country/countryLayout/layout-page/layout-page.ts
+++ src/app/country/countryLayout/layout-page/layout-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CountryNavBar } from "../../components/country-nav-bar/country-nav-bar";
 
 @Component({
   selector: 'app-layout-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CountryNavBar],
   templateUrl: './layout-page.html',
   styles: ``,
 })
